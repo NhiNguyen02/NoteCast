18f623f61b95fd0ff3c6bdbb183713dc7d717acf
bichnhan2701 <truongtuelam43@gmail.com>
2025-11-18 15:00:35 +0700

    Update app with VAD pre-process

--- app/src/main/java/com/example/notecast/presentation/ui/MainAppScreen.kt
+++ app/src/main/java/com/example/notecast/presentation/ui/MainAppScreen.kt
@@ -3,11 +3,11 @@ package com.example.notecast.presentation.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
