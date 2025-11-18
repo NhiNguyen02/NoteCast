13f52ce4cf9e4c9ed045a59052bb54f6934c6561
NhiNguyen02 <nhinguyen200604@gmail.com>
2025-11-19 02:17:31 +0700

    Rebase

--- app/src/main/java/com/example/notecast/presentation/ui/dialog/RenameFolderDialog.kt
+++ app/src/main/java/com/example/notecast/presentation/ui/dialog/RenameFolderDialog.kt
@@ -130,7 +130,6 @@ fun RenameFolderDialog(
 @Composable
 fun PreviewRenameFolderDialog() {
     MaterialTheme {
-        // Dữ liệu giả lập cho Preview
         val mockFolder = Folder(
             id = "1",
             name = "Công việc",
